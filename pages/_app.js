65a4220a66def0a61748bab8b8a39522798d425f
Smuktha <mukthas701@gmail.com>
2025-07-31 11:10:20 +0530

    Final portfolio build

--- pages/_app.js
+++ pages/_app.js
@@ -12,27 +12,31 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
-      <Head>
-        <title>Muktha Suvarna | Full Stack Developer</title>
-        <meta
-          name="description"
-          content="Portfolio of Muktha Suvarna - Full Stack Developer passionate about building sleek websites and creative software solutions."
-        />
-        <meta
-          name="keywords"
-          content="Muktha Suvarna, Web Developer, Portfolio, React, Next.js, Tailwind, Full Stack"
-        />
-        <meta name="author" content="Muktha Suvarna" />
-        <meta property="og:title" content="Muktha Suvarna | Portfolio" />
-        <meta
-          property="og:description"
-          content="Check out my developer portfolio and projects."
-        />
-        <meta property="og:image" content="/cover-image.jpg" />
-        <meta property="og:url" content="https://portfolio-mukthas-projects-7e55e4c8.vercel.app/" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+     <Head>
+  <title>Muktha Suvarna | Full Stack Developer</title>
+  <meta
+    name="description"
+    content="Portfolio of Muktha Suvarna - Full Stack Developer passionate about building sleek websites and creative software solutions."
+  />
+  <meta
+    name="keywords"
+    content="Muktha Suvarna, Web Developer, Portfolio, React, Next.js, Tailwind, Full Stack"
+  />
+  <meta name="author" content="Muktha Suvarna" />
+  <meta property="og:title" content="Muktha Suvarna | Portfolio" />
+  <meta
+    property="og:description"
+    content="Check out my developer portfolio and projects."
+  />
+  <meta property="og:image" content="/cover-image.jpg" />
+  <meta property="og:url" content="https://portfolio-mukthas-projects-7e55e4c8.vercel.app/" />
+  <meta name="twitter:card" content="summary_large_image" />
+
+  {/* ✅ Google site verification */}
+  <meta name="google-site-verification" content="EpWRlk_V3guk5RGAQWLZOZmKS9pUOKXGd7iMve8f9fo" />
+
+  <link rel="icon" href="/favicon.ico" />
+</Head>
 
       <CursorTrail />
       <Component {...pageProps} />
